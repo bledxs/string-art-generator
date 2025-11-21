369f29664d1b98246e9381db250c089e1740597e
Luis C. Rojas <luisc.rojas@hotmail.com>
2025-11-21 12:21:12 -0500

    feat: agregar reglas de ESLint para mejorar la accesibilidad y la consistencia del código

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,12 @@ export default [
     ignores: ['.next/**', 'node_modules/**', 'out/**', '.vercel/**'],
   },
   ...nextPlugin,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      'import/no-anonymous-default-export': 'off',
+      'jsx-a11y/alt-text': 'warn',
+      '@next/next/no-img-element': 'warn',
+    },
+  },
 ];
